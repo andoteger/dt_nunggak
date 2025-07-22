07a3165d090e934e449640ce36bf7ea1671073b4
andoteger <zubakyndo@gmail.com>
2025-07-22 10:35:50 +0700

    modified:   andotegerFlask.py 	renamed:    asset/json/DT_AKTIF_TIDAK.json -> asset/DT_AKTIF_TIDAK.json 	renamed:    asset/json/DT_NUNGGAK.json -> asset/DT_NUNGGAK.json 	renamed:    asset/json/KREDIT_BEREDAR.json -> asset/KREDIT_BEREDAR.json 	renamed:    asset/json/PENABUNG.json -> asset/PENABUNG.json 	deleted:    asset/json/hasil_gabungan.json 	new file:   asset/json/{file}.json

--- andotegerFlask.py
+++ andotegerFlask.py
@@ -123,45 +123,45 @@ def konversi():
         print(f"File {file} berhasil di konversi ke {json_file}")
 
 
-    # Baca DT_NUNGGAK dengan dtype yang ditentukan
-    data_nunggak = pd.read_excel(
-        'asset/DT_NUNGGAK.xlsx',
-        dtype={
-            'MDLBRCO': str,
-            'MDLDLRF': str,
-            'CSWREFN': str,
-            'NOREK_SIMP': str,
-            'KD_ANALIS': str,
-            'MDLSCLS': str,
-        },
-        engine='openpyxl'
-    )
+    # # Baca DT_NUNGGAK dengan dtype yang ditentukan
+    # data_nunggak = pd.read_excel(
+    #     'asset/DT_NUNGGAK.xlsx',
+    #     dtype={
+    #         'MDLBRCO': str,
+    #         'MDLDLRF': str,
+    #         'CSWREFN': str,
+    #         'NOREK_SIMP': str,
+    #         'KD_ANALIS': str,
+    #         'MDLSCLS': str,
+    #     },
+    #     engine='openpyxl'
+    # )
 
-    data_penabung = pd.read_excel(
-    'asset/PENABUNG.xlsx',
-    dtype={
-        'CSWREFN': str,
-        'ACTACCU': str,
-    },
-    engine='openpyxl'
-    )
+    # data_penabung = pd.read_excel(
+    # 'asset/PENABUNG.xlsx',
+    # dtype={
+    #     'CSWREFN': str,
+    #     'ACTACCU': str,
+    # },
+    # engine='openpyxl'
+    # )
 
-    # Gabungkan data dan pastikan ACTACCU adalah string
-    merged_data = pd.merge(
-        data_nunggak,
-        data_penabung[['CSWREFN', 'ACTACCU']],
-        on='CSWREFN',
-        how='left'
-    ).fillna({'ACTACCU': "kosong"})  # Handle NaN untuk ACTACCU
+    # # Gabungkan data dan pastikan ACTACCU adalah string
+    # merged_data = pd.merge(
+    #     data_nunggak,
+    #     data_penabung[['CSWREFN', 'ACTACCU']],
+    #     on='CSWREFN',
+    #     how='left'
+    # ).fillna({'ACTACCU': "kosong"})  # Handle NaN untuk ACTACCU
 
-    # Konversi ke JSON (otomatis handle NaN sebagai null)
-    result = merged_data.to_dict('records')
+    # # Konversi ke JSON (otomatis handle NaN sebagai null)
+    # result = merged_data.to_dict('records')
 
-    # Simpan ke file JSON
-    with open('asset/hasil_gabungan.json', 'w', encoding='utf-8') as f:
-        json.dump(result, f, ensure_ascii=False, indent=4)
+    # # Simpan ke file JSON
+    # with open('asset/hasil_gabungan.json', 'w', encoding='utf-8') as f:
+    #     json.dump(result, f, ensure_ascii=False, indent=4)
 
-    print("Data berhasil digabungkan dan disimpan dalam hasil_gabungan.json")
+    # print("Data berhasil digabungkan dan disimpan dalam hasil_gabungan.json")
 
 def main():
     while True:
